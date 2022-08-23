c36a40cf1782aac2712c9a2624886d3922f24bbc
iSaacSigei <sigei.isack@student.moringaschool.com>
2022-08-23 10:11:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-const totalBatteries = batteryBatches.reduce((total, batch) => {
-    return total + batch;
-  } , 0);
+const totalBatteries = batteryBatches.reduce(addBatteries, 0);
+function addBatteries(total,batches){
+  return total+=batches;
+}
